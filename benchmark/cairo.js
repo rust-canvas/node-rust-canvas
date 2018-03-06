7eae7716a9209f3f2d716870a0fbb5493b3d837d
LongYinan <lynweklm@gmail.com>
2018-03-06 20:58:32 +0800

    bench: add with-image-async bench suite

--- benchmark/cairo.js
+++ benchmark/cairo.js
@@ -1,9 +1,14 @@
+const fs = require('fs')
+const { join } = require('path')
 const Canvas = require('canvas')
 
 const canvas = new Canvas(1920 * 2, 1080 * 2)
+const image = new Canvas.Image(2481, 1773)
+image.src = fs.readFileSync(join(process.cwd(), 'examples', 'fixtures', 'image.png'))
 
 module.exports = {
   canvas,
   ctx: canvas.getContext('2d'),
+  image,
   type: 'cairo'
 }
--- benchmark/skia.js
+++ benchmark/skia.js
@@ -1,9 +1,14 @@
-const { CanvasElement } = require('../lib/canvas-element')
+const fs = require('fs')
+const { join } = require('path')
+const { CanvasElement, ImageElement } = require('../lib')
 
 const canvas = new CanvasElement(1920 * 2, 1080 * 2)
+const image = new ImageElement(2481, 1773)
+image.src = fs.readFileSync(join(process.cwd(), 'examples', 'fixtures', 'image.png'))
 
 module.exports = {
   canvas,
   ctx: canvas.getContext('2d'),
+  image,
   type: 'skia',
 }
