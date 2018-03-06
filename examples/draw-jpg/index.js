450b6ca04d51e1d37b9b9078804951677ef9f785
yorkyao <yao.yao@arkie.ai>
2018-03-06 18:42:41 +0800

    refactor: improve examples

--- examples/draw-jpg/index.js
+++ examples/draw-jpg/index.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const { join, resolve } = require('path')
 
-const { CanvasElement } = require('../lib')
+const { CanvasElement, ImageElement } = require('../../lib')
 
-const canvas = new CanvasElement(1920 * 2, 1080 * 2)
+const canvas = new CanvasElement(1920, 1080)
 
 const ctx = canvas.getContext('2d')
 
+const image = new ImageElement(2560, 2560)
+image.src = fs.readFileSync(join(process.cwd(), 'examples', 'fixtures', 'image.jpg'))
+
+ctx.drawImage(image, 0, 0)
 ctx.lineWidth = 20
 ctx.strokeStyle = 'rgb(190,246,122)'
 ctx.moveTo(200, 200)
@@ -21,7 +25,7 @@ ctx.strokeStyle = '#f48fb1'
 ctx.lineWidth = 4
 ctx.strokeText('From Azure', 1600, 1400)
 
-const base64 = canvas.toDataURL('image/png')
-fs.writeFileSync(join(process.cwd(), 'examples', 'to-png.png'), base64.split(',')[1], {
+const base64 = canvas.toDataURL('image/jpeg', 0.1);
+fs.writeFileSync(join(process.cwd(), 'examples', 'draw-jpg', 'result.jpg'), base64.split(',')[1], {
   encoding: 'base64'
 })
